634298392bb7c9207cd043afbb86b073f36f3443
jlb30350 <112830521+jlb30350@users.noreply.github.com>
2024-11-13 12:04:11 +0100

    Update script.js

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,38 +1,42 @@
 $(function () {
-  $(".navbar a, footer a").on("click", function (event) {
-    event.preventDefault();
-    var hash = this.hash;
+    // Smooth scrolling for internal links in navbar and footer
+    $(".navbar a, footer a").on("click", function (event) {
+        event.preventDefault();
+        var hash = this.hash;
 
-    $("body").animate({ scrollTop: $(hash).offset().top }, 1, function () {
-      window.location.hash = hash;
-    })
-  });
-
-  $("#contact-form").submit(function (e) {
-    e.preventDefault();
-    $(".comments").empty();
-    var postdata = $("#contact-form").serialize();
-    console.log(postdata)
-    $.ajax({
-      type: "POST",
-      url: "php/contact.php",
-      data: postdata,
-      postdata: "firstname=&name=&email=&phone=&message=",
-      dataType: "json",
-      success: function (result) {
-        if (result.isSuccess) {
-          $("#contact-form").append(
-            "<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>"
-          );
-          $("#contact-form")[0].reset();
-        } else {
-          $("#firstname + .comments").html(result.firstnameError);
-          $("#name + .comments").html(result.nameError);
-          $("#email + .comments").html(result.emailError);
-          $("#phone + .comments").html(result.phoneError);
-          $("#message + .comments").html(result.messageError);
+        if (hash) {
+            $("html, body").animate(
+                { scrollTop: $(hash).offset().top },
+                500 // Durée de l'animation en millisecondes
+            );
         }
-      },
     });
-  });
+
+    // Form submission handling
+    $("#contact-form").submit(function (e) {
+        e.preventDefault();
+        $(".comments").empty();
+        var postdata = $("#contact-form").serialize();
+
+        $.ajax({
+            type: "POST",
+            url: "php/contact.php", // Assurez-vous que l'URL du script PHP est correcte
+            data: postdata,
+            dataType: "json",
+            success: function (result) {
+                if (result.isSuccess) {
+                    $("#contact-form").append(
+                        "<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>"
+                    );
+                    $("#contact-form")[0].reset();
+                } else {
+                    $("#firstname + .comments").html(result.firstnameError);
+                    $("#name + .comments").html(result.nameError);
+                    $("#email + .comments").html(result.emailError);
+                    $("#phone + .comments").html(result.phoneError);
+                    $("#message + .comments").html(result.messageError);
+                }
+            },
+        });
+    });
 });
